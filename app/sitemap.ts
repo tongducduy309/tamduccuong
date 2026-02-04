0bd01f96961969baf142d00f45ef7a487cfda438
tongducduy309 <119146803+tongducduy309@users.noreply.github.com>
2026-02-04 11:23:17 +0700

    update seo

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -6,7 +6,10 @@ export default function sitemap(): MetadataRoute.Sitemap {
   return [
     { url: `${base}/`, lastModified: new Date() },
     // thêm các trang bạn có:
-    // { url: `${base}/about`, lastModified: new Date() },
-    // { url: `${base}/blog`, lastModified: new Date() },
+    { url: `${base}/home`, lastModified: new Date() },
+    { url: `${base}/about`, lastModified: new Date() },
+    { url: `${base}/blogs`, lastModified: new Date() },
+    { url: `${base}/quotation`, lastModified: new Date() },
+    { url: `${base}/contact`, lastModified: new Date() },
   ];
 }
